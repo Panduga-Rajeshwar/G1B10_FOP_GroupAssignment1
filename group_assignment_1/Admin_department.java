d38fbee9ca625af07b63a8d325a1aba9bfcbd72d
Panduga-Rajeshwar <153173482+Panduga-Rajeshwar@users.noreply.github.com>
2023-12-21 03:18:47 +0530

    Admin_department.java

---
+++ group_assignment_1/Admin_department.java
@@ -0,0 +1,25 @@
+package group_assignment_1;
+
+public class Admin_Department extends Super_Department {
+	
+	//additional methods for admin department class
+	
+	public String department_Name() {
+		
+		return "Admin Department";
+		
+	}
+	
+	public String getTodaysWork() {
+		
+		return "Complete your documents submission";
+		
+	}
+	
+	public String getWorkDeadline() {
+		
+		return "Complete by EOD";
+		
+	}
+	
+}
